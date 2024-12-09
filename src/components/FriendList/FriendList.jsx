c50baecc669bcaf48ab372a7d4b72f94d6d6a27d
Andrij P. <andrejpopkov565@gmail.com>
2024-12-09 19:21:33 +0300

    add components FriendListItem

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,34 +1,16 @@
 import frStyles from "./FriendList.module.css";
-import clsx from "clsx";
+import FriendListItem from "../FriendListItem/FriendListItem";
 
 export default function FriendList({ friends }) {
   return (
     <ul className={frStyles["user-list"]}>
       {friends.map((friend) => (
-        <li key={friend.id}>
-          <div className={frStyles["user-container"]}>
-            <img src={friend.avatar} alt={friend.name} width="48" />
-            <p
-              style={{
-                fontWeight: 600,
-              }}
-            >
-              {friend.name}
-            </p>
-            <p
-              style={{
-                fontSize: 12,
-              }}
-              className={clsx(
-                friend.isOnline
-                  ? frStyles["status-online"]
-                  : frStyles["status-offline"]
-              )}
-            >
-              {friend.isOnline ? "Online" : "Offline"}
-            </p>
-          </div>
-        </li>
+        <FriendListItem
+          key={friend.id}
+          avatar={friend.avatar}
+          name={friend.name}
+          isOnline={friend.isOnline}
+        />
       ))}
     </ul>
   );
